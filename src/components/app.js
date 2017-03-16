10726e09e3a4c0b8104c5f1f36a42669ba83d66e
sami4thewin <samiomarmohammed@gmail.com>
2017-03-16 06:46:07 -0400

    Ran into some issues but done

--- src/components/app.js
+++ src/components/app.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import UserBlackjack  from './user_blackjack';
 import AIBlackjack  from './ai_blackjack';
+import { hitAi, hitUser } from '../actions/blackjack_actions'
 
 export default class App extends React.Component {
 
@@ -13,32 +14,55 @@ export default class App extends React.Component {
     this.stay = this.stay.bind(this)
   }
 
-  hitMe() {
-
+  hitMe(user) {
+    if (user === "user") {
+      this.props.store.dispatch(hitUser(this.props.store.getState().deck))
+    }else {
+      this.props.store.dispatch(hitAi(this.props.store.getState().deck))
+    }
   }
 
-  calculateAiScore() {
-    const aiScore = this.props.store.getState().aiCards.reduce((a, b) => {
-      return a.value += b.value
-    })
-    return aiScore
+  calculateAiScore(winner) {
+    const aiScore = this.props.store.getState().aiCards.reduce((preVal, currVal) => {
+                      return preVal += currVal.value
+                    }, 0)
+    // if (aiScore > 21) {
+    //   return "BUST"
+    // } else if (this.calculateUserScore() === "BUST") {
+    //   return "Winner!"
+    // } else {
+      return aiScore
+    // }
   }
 
-  calculateUserScore() {
-    const userScore = this.props.store.getState().userCards.reduce((a, b) => {
-      return a.value += b.value
-    })
-    return userScore
+  calculateUserScore(winner) {
+    const userScore = this.props.store.getState().userCards.reduce((preVal, currVal) => {
+            return preVal += currVal.value
+          }, 0)
+    // if (userScore > 21) {
+    //   return "BUST"
+    // } else if (this.calculateAiScore() === "BUST") {
+    //   return "Winner!"
+    // } else {
+      return userScore
+    // }
   }
 
   stay() {
-
+    // if (this.calculateUserScore() !== "BUST") {
+    //   while (this.calculateAiScore() < this.calculateUserScore()) {
+    //     if (this.calculateAiScore() === "BUST") {
+    //       return
+    //     }
+    //     this.hitMe("ai")
+    //   }
+    // }
   }
 
   render() {
     return(
       <div>
-        <UserBlackjack score={this.calculateUserScore}/>
+        <UserBlackjack score={this.calculateUserScore} userCards={this.props.store.getState().userCards} hitMe={this.hitMe} stay={this.stay}/>
         <AIBlackjack score={this.calculateAiScore} aiCards={this.props.store.getState().aiCards}/>
       </div>
     )
